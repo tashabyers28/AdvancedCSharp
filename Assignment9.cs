6912b03496ccabee413682d125c107936c82ffb5
Tasha B <56890015+tashabyers28@users.noreply.github.com>
2021-05-07 03:13:37 -0500

    Update Assignment9.cs

--- Assignment9.cs
+++ Assignment9.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.Dynamic;
 using Amazon.Lambda.Core;
-//using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.APIGatewayEvents;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -17,19 +17,12 @@ namespace Assignment9
     {
         public static readonly HttpClient client = new HttpClient();
 
-        public async Task<ExpandoObject> FunctionHandler(string input, ILambdaContext context)
+        public async Task<ExpandoObject> FunctionHandler(APIGatewayProxyRequest input, ILambdaContext context)
         {
-            HttpResponseMessage response = await client.GetAsync("https://api.nytimes.com/svc/books/v3/lists.json");
-            response.EnsureSuccessStatusCode();
-            object responseBody = await response.Content.ReadAsStringAsync();
-            //string jsonText = JsonConvert.SerializeObject(responseBody);
-            //var obj = JsonConvert.DeserializeObject<ExpandoObject>(jsonText, new ExpandoObjectConverter());
-            //string strCust = JsonConvert.SerializeObject(responseBody, new ExpandoObjectConverter());
-            //string output = JsonConvert.SerializeObject(responseBody);
-            //return JsonConvert.DeserializeObject<ExpandoObject>(output);
-            return (ExpandoObject)responseBody;
-            //return obj;
-
+            string bookInput = Convert.ToString(input.Body);
+            string url = $@"https://api.nytimes.com/svc/books/v3/lists/current/{bookInput}.json?api-key=tZ1EIvr6jm64GpPWcD08RfoH9YWzXhEA";
+            string message = await client.GetStringAsync(url);
+            return JsonConvert.DeserializeObject<ExpandoObject>(message);
         }
     }
 }
